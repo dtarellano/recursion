b35cfa4bce38b079927519b6f7d114f328b6c5e7
David Arellano <dtarellano1@gmail.com>
2017-09-15 16:53:13 -0700

    getElementByClassName comeple

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+
+  function findClass (element) {
+    if (element.classList && element.classList.contains(className)) {
+      results.push(element)
+    }
+    if (element.hasChildNodes()) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        findClass(element.childNodes[i]);
+      }
+    }
+  }
+
+  findClass(document.body);
+  console.log(results)
+  return results;
 };
