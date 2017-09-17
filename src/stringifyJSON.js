a60a9a44e73e0c2d47a5d46b3f64844189718cd7
David Arellano <dtarellano1@gmail.com>
2017-09-17 10:57:02 -0700

    solved stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  // toString did not work with null so i created an if case
+  if (obj === null) {
+    return "null"
+  }
+  // will create a wrapping string
+  if (typeof obj === "string") {
+    return '"' + obj + '"'
+  }
+  // created sequence to stringify arrays
+  if (Array.isArray(obj)) {
+    var arrayResults = [];
+    if (!obj.length) {
+      return '[]';
+    }
+    for(var i = 0; i < obj.length; i++) {
+      arrayResults.push(stringifyJSON(obj[i]))
+    }
+      return '[' + arrayResults + ']'
+
+  }
+  // created sequence to strigify objects
+  // also learned about "constructor" to cofirm its its object because typeof will return "object" on an array and an object
+  if (obj.constructor === Object) {
+    var objResults = [];
+    for (var key in obj) {
+      if(obj[key] === undefined || typeof obj[key] === 'function') {
+        // was trying to figure out how to skip sequences and I came across continue statement
+        continue;
+      }
+      objResults.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]))
+    }
+    return '{' + objResults.join() + '}'
+  }
+
+  return obj.toString();
 };
